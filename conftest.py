30c96b37f558f79303291943ebed4a30612b3545
Lenny Truong <lenny@cs.stanford.edu>
2017-07-25 09:30:55 -0700

    Clear circuit cache before each test

--- conftest.py
+++ conftest.py
@@ -1,6 +1,17 @@
 # content of conftest.py
 import sys
+import pytest
 
 collect_ignore = ["setup.py"]
 if sys.version_info[0] < 3:
     collect_ignore.append("tests/mantle40/test_expressions/test_expressions.py")
+
+
+@pytest.fixture(autouse=True)
+def mantle_test():
+    """
+    Clear the circuit cache before running, allows name reuse across tests
+    without collisions
+    """
+    import magma.circuit
+    magma.circuit.__magma_clear_circuit_cache()

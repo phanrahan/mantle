0748a859ee31865c188f86a6180bceb468c62910
Pat Hanrahan <hanrahan@cs.stanford.edu>
2017-08-14 16:58:29 -0700

    Updated golds.

--- tests/mantle40/mux/test_mux.py
+++ tests/mantle40/mux/test_mux.py
@@ -1,8 +1,8 @@
 import os
 os.environ['MANTLE'] = 'lattice'
-from mantle import Mux2, Mux4, Mux8, Mux16, MuxN, Mux
+from mantle import Mux2, Mux4, Mux8, Mux16, Mux
 
-def test_mux():
+def test_muxn():
     mux = Mux2()
     assert str(mux.interface) == 'I : Bits(2), S : In(Bit) -> O : Out(Bit)'
 
@@ -15,9 +15,7 @@ def test_mux():
     mux = Mux16()
     assert str(mux.interface) == 'I : Bits(16), S : Bits(4) -> O : Out(Bit)'
 
-    mux = MuxN(4)
-    assert str(mux.interface) == 'I : Bits(4), S : Bits(2) -> O : Out(Bit)'
-
+def test_mux():
     mux = Mux(2,1)
-    assert str(mux.interface) == 'I : Bits(2), S : In(Bit) -> O : Out(Bit)'
+    assert str(mux.interface) == 'I0 : Bits(1), I1 : Bits(1), S : In(Bit) -> O : Bits(1)'
 

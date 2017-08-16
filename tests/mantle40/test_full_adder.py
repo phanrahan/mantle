a37a6640c1723377652cca1b59d7bc2a5dc28ce1
Lenny Truong <lenny@cs.stanford.edu>
2017-08-15 21:25:13 -0700

    Fix test command in full_adder

--- tests/mantle40/test_full_adder.py
+++ tests/mantle40/test_full_adder.py
@@ -3,8 +3,8 @@ from magma import *
 
 def test_full_adder():
     compile("build/test_full_adder_coreir", FullAdder, output="coreir")
-    magma_check_files_equal(__file__, "build/test_full_adder_coreir",
-            "gold/test_full_adder_coreir")
+    magma_check_files_equal(__file__, "build/test_full_adder_coreir.json",
+            "gold/test_full_adder_coreir.json")
 
 if __name__ == "__main__":
     test_full_adder()

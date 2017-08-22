175df8fd936a78fe1c95da4f41b777558aa75c22
Pat Hanrahan <hanrahan@cs.stanford.edu>
2017-08-22 15:30:51 -0700

    Updated verilog/gates.py to work properly for positional arguments. Removed residual bake test stuff.

--- mantle/verilog/gates.py
+++ mantle/verilog/gates.py
@@ -12,54 +12,54 @@ __all__ += ['Buf']
 def ins(n=2):
     l = []
     for i in range(n):
-        l.append('input')
+        l.append('')
         l.append(In(Bit))
     return l
 
 def DefineAnd(n=2):
-    return DeclareCircuit(*(['and', 'output', Out(Bit)] + ins(n)))
+    return DeclareCircuit(*(['and', '', Out(Bit)] + ins(n)))
 
 def And(n=2):
     return DefineAnd(n)()
 
 
 def DefineNAnd(n=2):
-    return DeclareCircuit(*(['nand', 'output', Out(Bit)] + ins(n)))
+    return DeclareCircuit(*(['nand', '', Out(Bit)] + ins(n)))
 
 def NAnd(n=2):
     return DefineNAnd(n)()
 
 
 def DefineOr(n=2):
-    return DeclareCircuit(*(['or', 'output', Out(Bit)] + ins(n)))
+    return DeclareCircuit(*(['or', '', Out(Bit)] + ins(n)))
 
 def Or(n=2):
     return DefineOr(n)()
 
 
 def DefineNOr(n=2):
-    return DeclareCircuit(*(['nor', 'output', Out(Bit)] + ins(n)))
+    return DeclareCircuit(*(['nor', '', Out(Bit)] + ins(n)))
 
 def NOr(n=2):
     return DefineNOr(n)()
 
 
 def DefineXOr(n=2):
-    return DeclareCircuit(*(['xor', 'output', Out(Bit)] + ins(n)))
+    return DeclareCircuit(*(['xor', '', Out(Bit)] + ins(n)))
 
 def XOr(n=2):
     return DefineXOr(n)()
 
 
 def DefineNXOr(n=2):
-    return DeclareCircuit(*(['nxor', 'output', Out(Bit)] + ins(n)))
+    return DeclareCircuit(*(['nxor', '', Out(Bit)] + ins(n)))
 
 def NXOr(n=2):
     return DefineNXOr(n)()
 
 
-Not = DeclareCircuit('not', 'output', Out(Bit), 'input', In(Bit))
-Buf = DeclareCircuit('buf', 'output', Out(Bit), 'input', In(Bit))
+Not = DeclareCircuit('not', '', Out(Bit), '', In(Bit))
+Buf = DeclareCircuit('buf', '', Out(Bit), '', In(Bit))
 
 
 

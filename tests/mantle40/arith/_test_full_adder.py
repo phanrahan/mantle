e43768795d84c1ecb1d12f0f138bf2bf7b721e51
Lenny Truong <lenny@cs.stanford.edu>
2017-08-21 22:46:54 -0600

    Update test_full_adder coreir gold, also asser the result of the file comparison

--- tests/mantle40/arith/_test_full_adder.py
+++ tests/mantle40/arith/_test_full_adder.py
@@ -3,7 +3,7 @@ from magma import *
 
 def test_full_adder():
     compile("build/test_full_adder_coreir", FullAdder, output="coreir")
-    magma_check_files_equal(__file__, "build/test_full_adder_coreir.json",
+    assert magma_check_files_equal(__file__, "build/test_full_adder_coreir.json",
             "gold/test_full_adder_coreir.json")
 
 if __name__ == "__main__":

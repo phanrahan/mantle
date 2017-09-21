530619c0fd12588abad5d8bae7cf80cf6c363c9d
Lenny Truong <lenny@cs.stanford.edu>
2017-09-21 13:35:43 -0700

    Add sub test

--- tests/test_coreir/test_arith.py
+++ tests/test_coreir/test_arith.py
@@ -1,6 +1,6 @@
 from magma import *
 from magma.testing import check_files_equal
-from mantle.coreir.arith import DefineAdd
+from mantle.coreir.arith import DefineAdd, DefineSub
 
 
 def test_add():
@@ -8,3 +8,10 @@ def test_add():
             output="coreir")
     assert check_files_equal(__file__,
             "build/test_define_add.json", "gold/test_define_add.json")
+
+
+def test_sub():
+    compile("build/test_define_sub", DefineSub(4, cin=True, cout=True),
+            output="coreir")
+    assert check_files_equal(__file__,
+            "build/test_define_sub.json", "gold/test_define_sub.json")

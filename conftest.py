4afb5e0b8adc694b35ddbea79c6379a055759728
Lenny Truong <lenny@cs.stanford.edu>
2017-09-26 17:12:16 -0700

    Remove clear circuit cache testing logic

--- conftest.py
+++ conftest.py
@@ -13,7 +13,5 @@ def mantle_test():
     Clear the circuit cache before running, allows name reuse across tests
     without collisions
     """
-    import magma.circuit
-    magma.circuit.__magma_clear_circuit_cache()
     import magma.config
     magma.config.set_compile_dir('callee_file_dir')

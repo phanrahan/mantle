e1ed125d0d9cde15706c57ac9465d5016db5b2d9
Lenny Truong <lenny@cs.stanford.edu>
2017-09-26 17:38:05 -0700

    Update golds for lut initialization changes

--- tests/test_common/gold/test_and_3.v
+++ tests/test_common/gold/test_and_3.v
@@ -1,6 +1,6 @@
 module And3 (input [2:0] I, output  O);
 wire  inst0_O;
-SB_LUT4 #(.LUT_INIT(8'h80)) inst0 (.I0(I[0]), .I1(I[1]), .I2(I[2]), .I3(1'b0), .O(inst0_O));
+SB_LUT4 #(.LUT_INIT(16'h8080)) inst0 (.I0(I[0]), .I1(I[1]), .I2(I[2]), .I3(1'b0), .O(inst0_O));
 assign O = inst0_O;
 endmodule
 

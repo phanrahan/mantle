29e2f722f781e95815d3a0f5c338847a0e9ed3b8
Lenny Truong <lenny@cs.stanford.edu>
2017-10-02 08:58:02 -0700

    Add down counter test

--- tests/test_common/test_counter.py
+++ tests/test_common/test_counter.py
@@ -3,7 +3,7 @@ from magma.simulator import PythonSimulator
 from magma.bit_vector import BitVector
 import os
 os.environ["MANTLE"] = "lattice"
-from mantle.common import DefineCounter
+from mantle.common import DefineCounter, DefineDownCounter, DefineUpDownCounter
 import types
 
 
@@ -47,14 +47,32 @@ def check(circuit, sim, number_of_cycles):
         next(sim)
 
 
+@coroutine
+def counter4_sim():
+    while True:
+        for i in range(0, 1<<4):
+            O = i
+            yield O
+
+
 def test_counter():
     Counter4 = DefineCounter(4)
 
-    @coroutine
-    def counter4_sim():
-        while True:
-            for i in range(0, 1<<4):
-                O = i
-                yield O
+    check(Counter4, counter4_sim(), 1<<4 * 2)
 
-    check(Counter4, counter4_sim(), 1<<4)
+@coroutine
+def down_counter4_sim():
+    # Counter starts at 0, then counts down
+    while True:
+        O = 0
+        yield O
+        for i in range((1<<4) - 1, 0, -1):
+            O = i
+            print(O)
+            yield O
+
+
+def test_downcounter():
+    DownCounter4 = DefineDownCounter(4)
+
+    check(DownCounter4, down_counter4_sim(), 1<<4 * 2)

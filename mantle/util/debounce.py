03135ff49152c06a774cac29d3b119ced1f17294
Pat Hanrahan <hanrahan@cs.stanford.edu>
2018-06-20 07:53:12 -0700

    Added getCurrentDefinitin to debounce.

--- mantle/util/debounce.py
+++ mantle/util/debounce.py
@@ -1,14 +1,15 @@
-from magma import  getCurrentDefinition, wire
+import magma as m
 from mantle import FF, And
 
 def debounce(a, slow):
-    CLK = getCurrentDefinition().CLK
-    ffa = FF(ce=True)
-    wire(slow, ffa.CE)
-    wire(CLK, ffa.CLK)
-    ffa(a)
-    ffb = FF(ce=True)
-    wire(slow, ffb.CE)
-    wire(CLK, ffb.CLK)
-    ffb(ffa.O)
+    CLK = m.getCurrentDefinition().CLKIN
+
+    ffa = FF(has_ce=True)
+    m.wire(CLK, ffa.CLK)
+    ffa(a, ce=slow)
+
+    ffb = FF(has_ce=True)
+    m.wire(CLK, ffb.CLK)
+    ffb(ffa.O, ce=slow)
+
     return And(2)(ffa.O, ffb.O)

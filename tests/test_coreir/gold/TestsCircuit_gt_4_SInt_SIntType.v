60ec2e4329430d4ea577d357e9b897a772903650
Lenny Truong <lenny@stanford.edu>
2018-06-28 18:18:16 -0700

    Move tests to coreir

---
+++ tests/test_coreir/gold/TestsCircuit_gt_4_SInt_SIntType.v
@@ -0,0 +1,15 @@
+module SGT4 (input [3:0] I0, input [3:0] I1, output  O);
+wire  inst0_out;
+coreir_sgt inst0 (.in0(I0), .in1(I1), .out(inst0_out));
+assign O = inst0_out;
+endmodule
+
+module TestsCircuit_gt_4_SInt_SIntType (input signed [3:0] I0, input signed [3:0] I1, output  O0, output  O1);
+wire  inst0_O;
+wire  inst1_O;
+SGT4 inst0 (.I0(I0), .I1(I1), .O(inst0_O));
+SGT4 inst1 (.I0(I0), .I1(I1), .O(inst1_O));
+assign O0 = inst0_O;
+assign O1 = inst1_O;
+endmodule
+

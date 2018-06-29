0a3b57e71596f2bbd86e732389d225e7a348891f
Lenny Truong <lenny@stanford.edu>
2018-06-28 17:33:54 -0700

    Test all operators in one test

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -18,7 +18,12 @@ op = namedtuple("op", ["name", "operator"])
     op(name="lsr", operator=">>"),
     op(name="asr", operator=None),
     op(name="add", operator="+"),
-    op(name="sub", operator="-")
+    op(name="sub", operator="-"),
+    op(name="eq", operator=None),  # TODO: overload ==, causes issues when == used instead of is
+    op(name="lt", operator="<"),
+    op(name="le", operator="<="),
+    op(name="gt", operator=">"),
+    op(name="ge", operator=">="),
 ])
 @pytest.mark.parametrize("N", [4])
 @pytest.mark.parametrize("T,TType", [(m.UInt, m.UIntType), (m.SInt, m.SIntType), (m.Bits, m.BitsType)])
@@ -32,53 +37,34 @@ def test_binary_op(op, N, T, TType):
             return x.__name__
         return str(x)
     _name = "TestsCircuit_" + "_".join(to_str(x) for x in (op.name, N, T, TType))
+    # List of comparison ops so we can special case them (output type and
+    # wiring 0)
+    comparisons = ["eq", "lt", "le", "gt", "ge"]
+    if op.name in comparisons:
+        out_T = m.Out(m.Bit)
+        expected_res_type = m.BitType
+    else:
+        out_T = m.Out(T(N))
+        expected_res_type = TType
     class TestCircuit(m.Circuit):
         name = _name
         IO = ["I0", m.In(T(N)), "I1", m.In(T(N)),
-              "O0", m.Out(T(N)), "O1", m.Out(T(N))]
+              "O0", out_T, "O1", out_T]
         @classmethod
         def definition(io):
             # Test using the method directly
             res = getattr(mantle, op.name)(io.I0, io.I1)
-            assert isinstance(res, TType), type(res)
+            assert isinstance(res, expected_res_type), type(res)
             m.wire(res, io.O0)
             # Test using the operator if it exists, otherwise wire 0 to O1
-            if op.operator is None or op.operator in ["-", "+"] and T == m.Bits:
-                m.wire(m.bits(0, N), io.O1)
+            if op.operator is None or op.name in ["sub", "add"] + comparisons and T == m.Bits:
+                if op.name in comparisons:
+                    m.wire(0, io.O1)
+                else:
+                    m.wire(m.bits(0, N), io.O1)
             else:
                 res_operator = eval(f"io.I0 {op.operator} io.I1")
                 m.wire(res_operator,io.O1)
-
-    m.compile(f'build/{_name}', TestCircuit)
-    assert check_files_equal(__file__,
-            f"build/{_name}.v", f"gold/{_name}.v")
-
-
-@pytest.mark.parametrize("op_name", [
-    "eq",
-    "lt",
-    "le",
-    "gt",
-    "ge",
-])
-@pytest.mark.parametrize("N", [4])
-@pytest.mark.parametrize("T", [m.UInt, m.SInt, m.Bits])
-def test_comparison_op(op_name, N, T):
-    op = getattr(mantle, op_name)
-    def to_str(x):
-        if callable(x):
-            return x.__name__
-        return str(x)
-    _name = "TestsCircuit_" + "_".join(to_str(x) for x in (op_name, N, T))
-    class TestCircuit(m.Circuit):
-        name = _name
-        IO = ["I0", m.In(T(N)), "I1", m.In(T(N)),
-              "O", m.Out(m.Bit)]
-        @classmethod
-        def definition(io):
-            res = op(io.I0, io.I1)
-            assert isinstance(res, m.BitType), type(res)
-            m.wire(res, io.O)
 
     m.compile(f'build/{_name}', TestCircuit)
     assert check_files_equal(__file__,

6e03006650d739d01f6a507941da099e3748f14f
Lenny Truong <lenny@stanford.edu>
2018-07-12 21:54:05 -0700

    Add missing decode.py

---
+++ mantle/common/decode.py
@@ -0,0 +1,34 @@
+import magma as m
+from mantle import LUT
+from mantle import EQ
+
+__all__  = ['DefineDecode', 'Decode', 'decode']
+
+@m.cache_definition
+def DefineDecode(i, n):
+    circ = m.DefineCircuit(f"Decode{i}{n}", "I", m.In(m.Bits(n)), "O", m.Out(m.Bit))
+    m.wire(circ.O, EQ(n)(circ.I, m.bits(i, n)))
+    m.EndDefine()
+    return circ
+
+def Decode(i, n, invert=False, **kwargs):
+    """
+    Decode the n-bit number i.
+
+    @return: 1 if the n-bit input equals i
+    """
+
+    assert n <= 8
+
+    if n <= 4:
+        i = 1 << i
+        if invert:
+            mask = (1 << (1<<n)) - 1
+            i = mask & (~i)
+        return m.uncurry(LUT(i, n, **kwargs))
+
+    return DefineDecode(i, n)()
+
+def decode(I, i, invert=False, **kwargs):
+    return Decode(i, len(I), invert=invert, **kwargs)(I)
+

f2a05104708bebfe2521a37a0cb4a4f0237409ae
David Durst <davidbdurst@gmail.com>
2018-07-31 15:19:40 -0700

    declaration of one bit, 1d linebuffer

---
+++ mantle/common/linebuffer.py
@@ -0,0 +1,23 @@
+from magma import *
+
+@cache_definition
+def OneBitOneDimensionalLineBuffer(pixel_per_clock: int,
+                                   window_width: int,
+                                   image_size: int,
+                                   output_stride: int,
+                                   origin: int):
+    """
+
+    :param pixel_per_clock: The number of pixels (bits in this case) that the
+    linebuffer receives as input each clock.
+    :param window_width: The size of the stencils that are emitted
+    :param image_size: The size of the complete, 1D image
+    :param output_stride: The distance between origins of consecutive stencils
+    in terms of numbers of pixels. A stride of 1 means that they are next
+    to each other.
+    :param origin: The index of the currently provided pixel
+    :return:
+    """
+    class _LB(Circuit):
+        name = "OneBitOneDimensionalLineBuffer_{}pxPerClock_{}windowWidth" \
+               "_{}imgSize_{}outputStride_{}origin"

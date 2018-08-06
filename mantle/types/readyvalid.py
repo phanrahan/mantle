39ce149e70db58161f824225f37265eda8e85f07
Ross <rdaly525@stanford.edu>
2018-08-05 22:02:36 -0700

    fixed issue with accidentally creating a tuple. Added a test for the flattend case

--- mantle/types/readyvalid.py
+++ mantle/types/readyvalid.py
@@ -1,17 +1,17 @@
-import magma as m
+from magma import *
 from collections import OrderedDict
 
 #Expects data to be "output" or not directional
 #Can specifify the name of the data and if you want the data 'flattened' in the tuple
 def ReadyValid(data,flatten=False,data_name="data"):
   vals = OrderedDict()
-  vals["ready"] = m.In(m.Bit),
-  vals["valid"] = m.Out(m.Bit),
+  vals["ready"] = In(Bit)
+  vals["valid"] = Out(Bit)
   if flatten:
-    assert isinstance(a,m.TupleKind)
+    assert isinstance(data,TupleKind)
     for k in data.Ks:
       assert k != "ready" and k != "valid"
-      vals[k] = data[k]
+      vals[k] = Out(data[k])
   else:
-    vals[data_name] = m.Out(data)
-  return m.Tuple(vals)
+    vals[data_name] = Out(data)
+  return Tuple(vals)

fd7b6c826f8ee73592d260209f0a3ac3a5a2e166
Ross <rdaly525@stanford.edu>
2018-08-05 21:53:43 -0700

    added extra options for ready valid

--- mantle/types/readyvalid.py
+++ mantle/types/readyvalid.py
@@ -1,9 +1,17 @@
 import magma as m
+from collections import OrderedDict
 
 #Expects data to be "output" or not directional
-def ReadyValid(data):
-  return m.Tuple(
-    ready=m.In(m.Bit),
-    valid=m.Out(m.Bit),
-    data=m.Out(data),
-  )
+#Can specifify the name of the data and if you want the data 'flattened' in the tuple
+def ReadyValid(data,flatten=False,data_name="data"):
+  vals = OrderedDict()
+  vals["ready"] = m.In(m.Bit),
+  vals["valid"] = m.Out(m.Bit),
+  if flatten:
+    assert isinstance(a,m.TupleKind)
+    for k in data.Ks:
+      assert k != "ready" and k != "valid"
+      vals[k] = data[k]
+  else:
+    vals[data_name] = m.Out(data)
+  return m.Tuple(vals)

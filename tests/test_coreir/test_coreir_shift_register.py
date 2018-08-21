49639e82fa8f70bd92817d7460e0216b7cebd906
Lenny Truong <lenny@stanford.edu>
2018-08-21 14:33:12 -0700

    Fixup coreir test for new simulator interface

--- tests/test_coreir/test_coreir_shift_register.py
+++ tests/test_coreir/test_coreir_shift_register.py
@@ -1,39 +1,33 @@
-from magma import *
 import magma as m
-
 from magma.simulator import PythonSimulator
-from magma.scope import Scope
-from magma.bit_vector import BitVector
-from magma.bitutils import int2seq, seq2int
-import mantle
 from mantle import DefineRegister
+
 
 def test_shift_register():
     N = 4
     Register4 = DefineRegister(4)
-    T = Bits(N)
+    T = m.Bits(N)
 
-    class ShiftRegister(Circuit):
+    class ShiftRegister(m.Circuit):
         name = "ShiftRegister"
-        IO = ["I", In(T), "O", Out(T), "CLK", In(Clock)]
+        IO = ["I", m.In(T), "O", m.Out(T), "CLK", m.In(m.Clock)]
         @classmethod
         def definition(io):
             regs = [Register4() for _ in range(N)]
-            wireclock(io, regs)
-            wire(io.I, regs[0].I)
-            fold(regs, foldargs={"I":"O"})
-            wire(regs[-1].O, io.O)
+            m.wire(io.I, regs[0].I)
+            m.fold(regs, foldargs={"I": "O"})
+            m.wire(regs[-1].O, io.O)
 
     simulator = PythonSimulator(ShiftRegister, clock=ShiftRegister.CLK)
     expected = [0, 0, 0] + list(range(0, 1 << N, 3))[:-3]
     actual = []
     for i in range(0, 1 << N, 3):
-        simulator.set_value(ShiftRegister.I, uint(i, N))
+        simulator.set_value(ShiftRegister.I, i)
         simulator.advance(2)
-        actual.append(seq2int(simulator.get_value(ShiftRegister.O)))
+        actual.append(simulator.get_value(ShiftRegister.O))
 
     assert actual == expected
 
     m.compile("build/ShiftRegister", ShiftRegister, output="coreir")
-    assert m.testing.check_files_equal(__file__,
-            "build/ShiftRegister.json", "gold/ShiftRegister.json")
+    assert m.testing.check_files_equal(__file__, "build/ShiftRegister.json",
+                                       "gold/ShiftRegister.json")

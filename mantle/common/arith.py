9d872ed9951d13ce8b9c76376d7323ee673c0b9c
Lenny Truong <lenny@stanford.edu>
2018-09-11 13:41:34 -0700

    Guard imports of division on other targets

--- mantle/common/arith.py
+++ mantle/common/arith.py
@@ -1,5 +1,5 @@
 import magma as m
-from mantle import DefineAdd, DefineSub, DefineNegate, DefineUDiv, DefineSDiv
+from mantle import DefineAdd, DefineSub, DefineNegate
 
 __all__ = [ "Add", "Sub", "Negate", "UDiv", "SDiv" ]
 
@@ -20,6 +20,9 @@ def UDiv(width, **kwargs):
     if m.mantle_target != "coreir":
         raise NotImplementedError(
             "Division not implemented for mantle target {m.mantle_target}")
+    # Workaround import error for other targets by delaying import until after
+    # if guard
+    from mantle import DefineUDiv
     return DefineUDiv(width)(**kwargs)
 
 
@@ -27,4 +30,7 @@ def SDiv(width, **kwargs):
     if m.mantle_target != "coreir":
         raise NotImplementedError(
             "Division not implemented for mantle target {m.mantle_target}")
+    # Workaround import error for other targets by delaying import until after
+    # if guard
+    from mantle import DefineSDiv
     return DefineSDiv(width)(**kwargs)

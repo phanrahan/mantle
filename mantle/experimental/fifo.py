de5c496e8c4a1fa4d3220a38cc4c40d375bcdbe9
Lenny Truong <lenny@stanford.edu>
2018-10-16 19:33:50 -0700

    Add test for fifo, fix bugs

--- mantle/experimental/fifo.py
+++ mantle/experimental/fifo.py
@@ -25,11 +25,11 @@ def DefineFIFO(height, width):
             deq_ptr_inc = m.uint(deq_ptr.O) + 1
             enq_ptr_inc = m.uint(enq_ptr.O) + 1
             is_full_next = mantle.mux([
-                m.bit(True), mantle.mux([m.bit(False), is_full.O], do_deq & is_full.O)],
-                do_enq & ~do_enq & (enq_ptr_inc == deq_ptr_inc))
+                mantle.mux([is_full.O, m.bit(False)], do_deq & is_full.O), m.bit(True)],
+                do_enq & ~do_deq & (enq_ptr_inc == deq_ptr.O))
 
-            enq_ptr(mantle.mux([enq_ptr_inc, enq_ptr.O], do_enq))
-            deq_ptr(mantle.mux([deq_ptr_inc, deq_ptr.O], do_deq))
+            enq_ptr(mantle.mux([enq_ptr.O, enq_ptr_inc], do_enq))
+            deq_ptr(mantle.mux([deq_ptr.O, deq_ptr_inc], do_deq))
 
             is_full(is_full_next)
             ram = mantle.DefineMemory(height, width)()
@@ -44,3 +44,23 @@ def DefineFIFO(height, width):
 
 FIFO4x8 = DefineFIFO(4, 8)
 m.compile('build/FIFO4x8', FIFO4x8)
+
+import fault
+
+
+tester = fault.Tester(FIFO4x8, FIFO4x8.CLK)
+
+for i in range(0, 4):
+    tester.poke(FIFO4x8.enq_val, 1)
+    tester.poke(FIFO4x8.enq_dat, i)
+    tester.step(2)
+
+tester.poke(FIFO4x8.enq_val, 0)
+
+for i in range(0, 4):
+    tester.poke(FIFO4x8.deq_rdy, 1)
+    tester.expect(FIFO4x8.deq_val, 1)
+    tester.expect(FIFO4x8.deq_dat, i)
+    tester.step(2)
+
+tester.compile_and_run(target="verilator")

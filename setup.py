014f4240e662700538cf2885e77e287b0736a70c
Lenny Truong <lenny@stanford.edu>
2018-12-04 13:57:49 -0800

    Bump fault version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mantle',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/phanrahan/mantle',
     license='MIT',
     maintainer='Lenny Truong',
@@ -26,7 +26,7 @@ setup(
 
     install_requires=[
         "six",
-        "fault>=0.20, <=0.35",
+        "fault>=0.20, <=0.36",
     ],
     python_requires='>=3.6'
 )

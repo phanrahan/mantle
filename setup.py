96316d5b4641cc0f9380d613e6facb81df7fab96
Lenny Truong <lenny@stanford.edu>
2019-01-25 10:21:38 -0800

    Update magma dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mantle',
-    version='0.1.9',
+    version='0.1.10',
     url='https://github.com/phanrahan/mantle',
     license='MIT',
     maintainer='Lenny Truong',
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         "six",
         "fault>=0.20, <=0.36",
-        "magma-lang==0.1.6"
+        "magma-lang>=0.1.6, <=0.1.7"
     ],
     python_requires='>=3.6'
 )

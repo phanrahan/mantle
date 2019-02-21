e4070b07ae2c392a295982a378e6f798f0de7971
Lenny Truong <lenny@stanford.edu>
2019-02-21 14:00:03 -0800

    Remove set_database_hash calls

--- tests/test_ice40/test_ramb.py
+++ tests/test_ice40/test_ramb.py
@@ -1,6 +1,5 @@
 from magma import *
 import magma as m
-m.config.set_database_hash_backend("verilog")
 from bit_vector import BitVector
 from mantle.lattice.ice40.RAMB import ROMB, RAMB
 from magma.simulator import PythonSimulator

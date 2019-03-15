3247b118f6dc2c70862c0e92812983088ad0cc05
Lenny Truong <leonardtruong@fb.com>
2019-03-15 15:22:03 -0700

    Fix regression from reset priority change

--- mantle/coreir/register.py
+++ mantle/coreir/register.py
@@ -27,7 +27,7 @@ def DefineRegister(n, init=0, has_ce=False, has_reset=False,
                 if has_ce:
                     I = mantle.mux([reg.O, I], io.CE, name="enable_mux")
                 if has_reset:
-                    I = mantle.mux([io.I, m.bits(init, n)], io.RESET)
+                    I = mantle.mux([I, m.bits(init, n)], io.RESET)
                 m.wire(I, reg.I)
                 m.wire(io.O, reg.O)
         return Register

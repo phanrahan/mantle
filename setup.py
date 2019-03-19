0b3a4480ad3d7a678023a2c9f69cd2ae34ac6853
Raj Setaluri <raj.setaluri@gmail.com>
2019-03-18 19:54:00 -0700

    Release v2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mantle',
-    version='0.1.16',
+    version='1.0.0',
     url='https://github.com/phanrahan/mantle',
     license='MIT',
     maintainer='Lenny Truong',

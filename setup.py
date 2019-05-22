c84502873292991817e4bde309e1f028ab8b935d
Lenny Truong <lenny@stanford.edu>
2019-05-22 08:27:48 -0700

    Release 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mantle',
-    version='1.0.3',
+    version='1.0.4',
     url='https://github.com/phanrahan/mantle',
     license='MIT',
     maintainer='Lenny Truong',

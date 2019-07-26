6242306ddbe0527ef473ada3023b142be3630fc6
Lenny Truong <leonardtruong@fb.com>
2019-07-26 16:24:49 -0700

    Release 1.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mantle',
-    version='1.0.7',
+    version='1.0.8',
     url='https://github.com/phanrahan/mantle',
     license='MIT',
     maintainer='Lenny Truong',

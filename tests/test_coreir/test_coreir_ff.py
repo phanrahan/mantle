f8e8b00ca2b7dc873bee546031ec39404d476d58
Lenny Truong <lenny@stanford.edu>
2019-11-13 17:25:48 -0800

    Revert change

--- tests/test_coreir/test_coreir_ff.py
+++ tests/test_coreir/test_coreir_ff.py
@@ -14,7 +14,7 @@ def test_ff():
 def test_ff_has_reset():
     DFF = DefineDFF(has_reset=True)
     print(repr(DFF))
-    compile("build/test_coreir_dff_reset", DFF, output="coreir-verilog")
+    compile("build/test_coreir_dff_reset", DFF, output="coreir")
     assert check_files_equal(__file__,
             "build/test_coreir_dff_reset.json", "gold/test_coreir_dff_reset.json")
 

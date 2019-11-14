c1da54cb239be84078169e1e92d3cbce51631f29
Lenny Truong <lenny@stanford.edu>
2019-11-13 17:25:01 -0800

    Fix bug when invalid name is automatically captured in debug mode

--- tests/test_coreir/test_coreir_ff.py
+++ tests/test_coreir/test_coreir_ff.py
@@ -1,3 +1,4 @@
+import magma as m
 from magma import *
 from magma.testing import check_files_equal
 from mantle.coreir import DefineDFF
@@ -13,7 +14,16 @@ def test_ff():
 def test_ff_has_reset():
     DFF = DefineDFF(has_reset=True)
     print(repr(DFF))
-    compile("build/test_coreir_dff_reset", DFF, output="coreir")
+    compile("build/test_coreir_dff_reset", DFF, output="coreir-verilog")
     assert check_files_equal(__file__,
             "build/test_coreir_dff_reset.json", "gold/test_coreir_dff_reset.json")
 
+
+def test_no_reg():
+    m.config.set_debug_mode(True)
+    DFF = DefineDFF(has_reset=True)
+    print(repr(DFF))
+    compile("build/test_coreir_dff_debug_no_reg", DFF, output="coreir-verilog")
+    assert check_files_equal(__file__,
+            "build/test_coreir_dff_debug_no_reg.json", "gold/test_coreir_dff_debug_no_reg.json")
+    m.config.set_debug_mode(False)

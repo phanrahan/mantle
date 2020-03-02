b1cab7d84c33911b8b7032b6608ee927a3199057
Rajsekhar Setaluri <rajsekhar@fb.com>
2020-03-02 11:42:51 -0800

    Update HalfAdder generation to new io syntax

--- mantle/common/halfadder.py
+++ mantle/common/halfadder.py
@@ -1,15 +1,14 @@
 import magma as m
 from mantle import XOr, And
 
+
 __all__ = [ "HalfAdder" ]
 
-class HalfAdder(m.Circuit):
-    IO = ["I", m.In(m.Bit), "CIN", m.In(m.Bit),
-          "O", m.Out(m.Bit), "COUT", m.Out(m.Bit)]
 
-    @classmethod
-    def definition(io):
-        # Generate the sum
-        m.wire( XOr(2)(io.I, io.CIN), io.O )
-        # Generate the carry
-        m.wire( And(2)(io.I, io.CIN), io.COUT )
+class HalfAdder(m.Circuit):
+    io = m.IO(I=m.In(m.Bit), CIN=m.In(m.Bit), O=m.Out(m.Bit), COUT=m.Out(m.Bit))
+
+    # Generate the sum
+    m.wire( XOr(2)(io.I, io.CIN), io.O )
+    # Generate the carry
+    m.wire( And(2)(io.I, io.CIN), io.COUT )

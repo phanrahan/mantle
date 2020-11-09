c2fc9c9381a9fa8a04dabf33e9c2d089b1fc9a14
Lenny Truong <lenny@stanford.edu>
2020-11-09 15:04:10 -0800

    Release 2.0.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mantle',
-    version='2.0.13',
+    version='2.0.14',
     url='https://github.com/phanrahan/mantle',
     license='MIT',
     author='Leonard Truong',
